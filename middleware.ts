40b01b2f778c688338028eb71398a9c933e354f5
Jacob Teddy <tetraphobia@wyvrn.dev>
2023-06-22 13:48:04 -0400

    feat: cors options

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,44 @@
 import { oak } from "./deps.ts";
+import type { CorsOptions } from "./types.ts";
 
-export async function simplecors(
-    context: oak.Context,
-    next: oak.Next,
+const DEFAULT_OPTIONS: CorsOptions = {
+    credentials: "true",
+    headers: "*",
+    methods: "*",
+    optionsSuccessCode: 204,
+    origin: "*",
+};
+
+/** Simple CORS middleware for oak. Provides bare minimum functionality (for now).
+ *
+ * ```ts
+ * import { Application } from "https://deno.land/x/oak/mod.ts";
+ * import { simplecors } from "mod.ts";
+ *
+ * const app  new Application();
+ * app.use(simplecors());
+ *
+ * await app.listen(":80");
+ * ```
+ */
+export function simplecors(
+    opts?: CorsOptions,
 ) {
-    const { request } = context;
-    request.headers.set("Access-Control-Allow-Origin", "*");
-    await next();
+    return (async (
+        { request, response }: oak.Context,
+        next: oak.Next,
+    ) => {
+        opts = { ...DEFAULT_OPTIONS, ...opts };
+
+        if (request.method.toUpperCase() === "OPTIONS") {
+            response.status = opts.optionsSuccessCode as oak.Status
+        }
+
+        response.headers.set("Access-Control-Allow-Origin", opts.origin!);
+        response.headers.set("Access-Control-Allow-Credentials", opts.credentials!);
+        response.headers.set("Access-Control-Allow-Methods", opts.methods!);
+        response.headers.set("Access-Control-Allow-Headers", opts.headers!)
+
+        await next();
+    });
 }
---
+++ types.ts
@@ -0,0 +1,7 @@
+export interface CorsOptions {
+    credentials?: string;
+    headers?: string;
+    methods?: string;
+    optionsSuccessCode?: number;
+    origin?: string;
+}
